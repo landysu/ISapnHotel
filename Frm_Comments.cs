75104adb7b45cbb36dbfad5a85f0855b1e36e910
landysu <landysu53@gmail.com>
2023-04-10 20:15:33 +0800

    第一次操作,匯入程式

---
+++ Frm_Comments.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Hotel
+{
+    public partial class Frm_Comments : Form
+    {
+        public Frm_Comments()
+        {
+            InitializeComponent();
+
+            this.dbHotel.Database.Log = Console.WriteLine;
+
+            dateTimePicker1.ResetText();
+            Load_RoomMembers();
+            Load_Rooms();
+        }
+        //===============================
+        //HotelOrderTestEntities2 dbHotel = new HotelOrderTestEntities2();//教室用DB
+        TestHotelOrderEntities dbHotel = new TestHotelOrderEntities();//家裡NB用
+        void Load_Rooms()
+        {
+            var q = from p in this.dbHotel.Rooms
+                    orderby p.RoomID
+                    select p.RoomID;
+
+            this.combxRoomId.DataSource = q.ToList();
+        }
+
+        void Load_RoomMembers()
+        {
+            var q = from p in this.dbHotel.RoomMembers
+                    orderby p.MemberID
+                    select p.MemberID;
+
+            this.combxMemberId.DataSource = q.ToList();
+        }
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            decimal Point;
+            bool N = decimal.TryParse(txtCommentPoint.Text, out Point);
+            if (N && Point > 0 && Point <= 5)
+            {
+                Comment comment = new Comment
+                {
+                    MemberID = combxMemberId.Text,
+                    RoomId = combxRoomId.Text,
+                    CommentDate = dateTimePicker1.Value,
+                    CommentPoint = Convert.ToDecimal(txtCommentPoint.Text), //轉小數點
+                    CommentDetail = txtCommentDetail.Text
+                };
+                this.dbHotel.Comments.Add(comment);
+                this.dbHotel.SaveChanges();
+
+                this.Read_RefreshDataGridView();
+            }
+            else
+            {
+                MessageBox.Show("回饋評點請輸入1-5之間的數字");
+            }
+        }
+        //修改
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //update
+            int commentid = Convert.ToInt32(this.dataGridView1.CurrentRow.Cells["CommentId"].Value);
+
+            var CommentUpDate = dbHotel.Comments.FirstOrDefault(rp => rp.CommentId == commentid);
+
+            if (CommentUpDate != null)
+            {
+                CommentUpDate.MemberID = combxMemberId.Text;
+                CommentUpDate.RoomId = combxRoomId.Text;
+                CommentUpDate.CommentPoint = Convert.ToDecimal(txtCommentPoint.Text);
+                CommentUpDate.CommentDetail = txtCommentDetail.Text;
+
+                dbHotel.SaveChanges();
+                this.Read_RefreshDataGridView();
+            }
+        }
+        //查詢:將資料載入到到dataGridView1
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var q = from c in this.dbHotel.Comments
+                    join m in this.dbHotel.RoomMembers
+                    on c.MemberID equals m.MemberID
+                    select new { c.CommentId, c.CommentDate, c.CommentPoint, c.CommentDetail, c.MemberID, m.MemberName, c.RoomId };
+
+            this.dataGridView1.DataSource = q.ToList();
+        }
+        //刪除一筆資料
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCellAddress.X > 0)
+            {
+                MessageBox.Show("請選擇要刪除的資料項", "刪除失敗", MessageBoxButtons.OK);
+                return;
+            }
+            var deleteRow = (from p in this.dbHotel.Comments.AsEnumerable()
+                             where p.CommentId == Convert.ToInt32(dataGridView1.CurrentCell.Value)
+                             select p).FirstOrDefault(); 
+            
+            DialogResult result = MessageBox.Show("確定要刪除資料嗎？", "提示", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                try
+                {
+                    if (deleteRow != null)
+                    {
+                        this.dbHotel.Comments.Remove(deleteRow);
+
+                        this.dbHotel.SaveChanges();
+
+                        this.Read_RefreshDataGridView();
+                        MessageBox.Show("已成功刪除資料");
+                    }
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    MessageBox.Show($"{ex}");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"{ex}");
+                }
+            }
+        }
+        //重新載入資料到dataGridView1
+        void Read_RefreshDataGridView()
+        {
+            this.dataGridView1.DataSource = null;
+            this.dataGridView1.DataSource = this.dbHotel.Comments.ToList();
+        }
+        //將游標所在資料放進畫面各欄位項目
+        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            int commentid = Convert.ToInt32(this.dataGridView1.CurrentRow.Cells["CommentId"].Value);
+                        
+            var o = from c in this.dbHotel.Comments
+                    join m in this.dbHotel.RoomMembers
+                    on c.MemberID equals m.MemberID
+                    where c.CommentId == commentid
+                    select new
+                    {
+                        c.CommentId,
+                        c.CommentDate,
+                        c.MemberID,
+                        m.MemberName,
+                        c.RoomId,
+                        c.CommentPoint,
+                        c.CommentDetail,
+                    };
+            foreach (var CommentId in o)
+            {
+                txtCommentId.Text = CommentId .CommentId.ToString();
+                //取得住宿時間並格式化
+                //DateTime intime = DateTime.ParseExact(IndateTimePicker.Value.ToString("yyyy-MM-dd") + " 14:00:00", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+                dateTimePicker1.Text = CommentId.CommentDate.Value.ToString("yyyy-MM-dd HH:mm:ss");
+                combxMemberId.Text = CommentId.MemberID;
+                txtMemberName.Text = CommentId.MemberName;
+                combxRoomId.Text = CommentId.RoomId;
+                txtCommentPoint.Text = Convert.ToString(CommentId.CommentPoint);
+                txtCommentDetail.Text = CommentId.CommentDetail;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtCommentId.Clear();
+            //dateTimePicker1.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            //dateTimePicker1.Text = Convert.ToString(DateTime.Now);
+            dateTimePicker1.ResetText();
+            Load_RoomMembers();
+            txtMemberName.Clear();
+            Load_Rooms();
+            txtCommentPoint.Clear();
+            txtCommentDetail.Clear();
+        }
+    }
+}
---
+++ Frm_Comments.designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Hotel
+{
+    partial class Frm_Comments
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCommentPoint = new System.Windows.Forms.TextBox();
+            this.txtCommentDetail = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.combxMemberId = new System.Windows.Forms.ComboBox();
+            this.combxRoomId = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCommentId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtMemberName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 220);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "回  饋  評  點 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 271);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "回  饋  評  價 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 142);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "會  員  編  號 :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 181);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(105, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "房  間  編  號 :";
+            // 
+            // txtCommentPoint
+            // 
+            this.txtCommentPoint.Location = new System.Drawing.Point(184, 222);
+            this.txtCommentPoint.Margin = new System.Windows.Forms.Padding(5);
+            this.txtCommentPoint.Name = "txtCommentPoint";
+            this.txtCommentPoint.Size = new System.Drawing.Size(164, 29);
+            this.txtCommentPoint.TabIndex = 7;
+            // 
+            // txtCommentDetail
+            // 
+            this.txtCommentDetail.Location = new System.Drawing.Point(184, 271);
+            this.txtCommentDetail.Margin = new System.Windows.Forms.Padding(5);
+            this.txtCommentDetail.Multiline = true;
+            this.txtCommentDetail.Name = "txtCommentDetail";
+            this.txtCommentDetail.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCommentDetail.Size = new System.Drawing.Size(385, 194);
+            this.txtCommentDetail.TabIndex = 9;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(84, 504);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 42);
+            this.btnInsert.TabIndex = 10;
+            this.btnInsert.Text = "新增";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(184, 504);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 42);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "修改";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(284, 504);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 42);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "查詢";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(384, 504);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 42);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "刪除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(589, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(715, 474);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick_1);
+            // 
+            // combxMemberId
+            // 
+            this.combxMemberId.FormattingEnabled = true;
+            this.combxMemberId.Location = new System.Drawing.Point(184, 142);
+            this.combxMemberId.Name = "combxMemberId";
+            this.combxMemberId.Size = new System.Drawing.Size(164, 28);
+            this.combxMemberId.TabIndex = 15;
+            // 
+            // combxRoomId
+            // 
+            this.combxRoomId.FormattingEnabled = true;
+            this.combxRoomId.Location = new System.Drawing.Point(184, 182);
+            this.combxRoomId.Name = "combxRoomId";
+            this.combxRoomId.Size = new System.Drawing.Size(164, 28);
+            this.combxRoomId.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "回饋評價編號 :";
+            // 
+            // txtCommentId
+            // 
+            this.txtCommentId.Enabled = false;
+            this.txtCommentId.Location = new System.Drawing.Point(184, 60);
+            this.txtCommentId.Margin = new System.Windows.Forms.Padding(5);
+            this.txtCommentId.Name = "txtCommentId";
+            this.txtCommentId.Size = new System.Drawing.Size(164, 29);
+            this.txtCommentId.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 103);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "評  價  日  期 :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(184, 101);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2023, 6, 30, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(164, 29);
+            this.dateTimePicker1.TabIndex = 20;
+            this.dateTimePicker1.Value = new System.DateTime(2023, 4, 1, 0, 0, 0, 0);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(84, 423);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 42);
+            this.btnClear.TabIndex = 21;
+            this.btnClear.Text = "清除";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtMemberName
+            // 
+            this.txtMemberName.Enabled = false;
+            this.txtMemberName.Location = new System.Drawing.Point(356, 142);
+            this.txtMemberName.Margin = new System.Windows.Forms.Padding(5);
+            this.txtMemberName.Name = "txtMemberName";
+            this.txtMemberName.Size = new System.Drawing.Size(164, 29);
+            this.txtMemberName.TabIndex = 22;
+            // 
+            // Frm_Comments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 749);
+            this.Controls.Add(this.txtMemberName);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCommentId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combxRoomId);
+            this.Controls.Add(this.combxMemberId);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtCommentDetail);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCommentPoint);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Frm_Comments";
+            this.Text = "Frm_Comments";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCommentPoint;
+        private System.Windows.Forms.TextBox txtCommentDetail;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox combxMemberId;
+        private System.Windows.Forms.ComboBox combxRoomId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCommentId;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtMemberName;
+    }
+}
+
